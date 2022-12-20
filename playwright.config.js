23af5eee832ed71989e500253c09a3b06c41a933
Joe Risner <joseph@aumni.fund>
2022-12-20 10:40:05 -0700

    Add cross origin iframe and playwright test

--- playwright.config.js
+++ playwright.config.js
@@ -3,33 +3,30 @@ const config = {
     timeout: 5000, // Maximum time expect() should wait for the condition to be met
   },
   fullyParallel: true,
-  outputDir: "./tests/artifacts/", // Folder for test artifacts such as screenshots, videos, traces, etc.
-  reporter: [
-    ["dot"],
-    ["html", { open: "never", outputFolder: "./tests/report" }],
-  ],
-  testDir: "./tests",
+  outputDir: './tests/artifacts/', // Folder for test artifacts such as screenshots, videos, traces, etc.
+  reporter: [['dot'], ['html', { open: 'never', outputFolder: './tests/report' }]],
+  testDir: './tests',
   timeout: 30 * 1000,
   use: {
     actionTimeout: 0, // Maximum time each action such as `click()` can take. 0 = no limit
-    baseURL: "http://localhost:4000",
-    screenshot: "only-on-failure",
-    trace: "on-first-retry", // Collect trace when retrying the failed test
+    baseURL: 'http://localhost:4000',
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry', // Collect trace when retrying the failed test
     viewport: { width: 1600, height: 1000 },
   },
   projects: [
     {
-      name: "Google Chrome",
+      name: 'Google Chrome',
       use: {
-        channel: "chrome",
+        channel: 'chrome',
       },
     },
   ],
-  webServer: {
-    command: "yarn start",
-    timeout: 120 * 1000,
-    url: "http://localhost:4000",
-  },
+  // webServer: {
+  //   command: "yarn start",
+  //   timeout: 120 * 1000,
+  //   url: "http://localhost:4000",
+  // },
 };
 
 export default config;
--- tests/iframe.spec.js
+++ tests/iframe.spec.js
@@ -1,13 +1,16 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Iframe', () => {
-  test('performs actions on elements embedded elements in an iframe', async ({ page }) => {
-    const iframe = page.frameLocator('[data-testid=myIframe]');
+  test('asserts heading text outside of iframes', async ({ page }) => {
+    await page.goto('/');
+    await expect(page.getByTestId('externalMessage')).toHaveText('Testing iframes');
+  });
+
+  test('performs actions in same-origin iframe', async ({ page }) => {
+    const iframe = page.frameLocator('[data-testid=sameOriginFrame]');
 
     await page.goto('/');
-    await expect(page.getByTestId('externalMessage')).toHaveText('Heading outside iframe');
-
-    await expect(iframe.getByTestId('iframeHeading')).toHaveText('Heading inside iframe');
+    await expect(iframe.getByTestId('iframeHeading')).toHaveText('Heading');
     await expect(iframe.getByAltText('Girl playing CTF')).toBeVisible();
 
     await iframe.getByPlaceholder('Search').fill('Lorem Ipsum');
@@ -17,5 +20,19 @@ test.describe('Iframe', () => {
     page.on('dialog', () => {
       expect(dialog.message()).toEqual('hello from alert');
     });
+  });
+
+  test('performs actions in cross-origin iframe', async ({ page }) => {
+    const iframe = page.frameLocator('[data-testid=crossOriginFrame]');
+    const inputElement = iframe.getByPlaceholder('What needs to be done?');
+    const todoList = iframe.locator('.todo-list');
+
+    await page.goto('/');
+    await expect(iframe.locator('header h1')).toHaveText('todos');
+    await expect(todoList).not.toBeVisible();
+
+    await inputElement.fill('Lorem Ipsum');
+    await inputElement.press('Enter');
+    await expect(todoList).toBeVisible();
   });
 });
